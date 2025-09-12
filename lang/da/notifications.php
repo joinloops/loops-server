680091cd4cc947d031aa6ab263b8244e6eb04a87
dansup <danielsupernault@gmail.com>
2025-09-12 01:32:36 -0600

    New translations notifications.php (Danish)

---
+++ lang/da/notifications.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'allCaughtUp' => 'You\'re all caught up!',
+    'errorLoadingNotifications' => 'Error loading notifications',
+    'messageTypes' => [
+        'videoLike' => 'liked your video',
+        'newFollower' => 'started following you',
+        'videoComment' => 'commented on your video',
+        'videoShare' => 'shared your video',
+        'default' => 'interacted with your content',
+    ],
+    'noNotifications' => 'No notifications',
+];
