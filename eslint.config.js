40c3c2ef2233729855a69db6400a116903584eac
Shlee <github@shl.ee>
2025-09-13 16:54:35 +0930

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,13 +4,10 @@ import globals from "globals";
 import tseslint from 'typescript-eslint'
 
 export default [
-  // Base configs
   js.configs.recommended,
-  
-  // Vue-specific configuration
+  ...vue.configs['flat/essential'],
   {
-    files: ['**/*.vue'],
-    ...vue.configs['flat/essential'][0], // Get the Vue config
+    files: ['resources/**/*.{js,vue}'],
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
@@ -33,49 +30,9 @@ export default [
       'no-unused-vars': 'warn'
     }
   },
-  
-  // TypeScript files only
-  {
-    files: ['resources/**/*.ts'],
-    ...tseslint.configs.recommended[0],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        process: 'readonly',
-        import: 'readonly',
-        route: 'readonly',
-        axios: 'readonly'
-      }
-    },
-    rules: {
-      'no-console': 'off',
-      'no-debugger': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn'
-    }
-  },
-  
-  // JavaScript files
-  {
-    files: ['resources/**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        process: 'readonly',
-        import: 'readonly',
-        route: 'readonly',
-        axios: 'readonly'
-      }
-    },
-    rules: {
-      'no-console': 'off',
-      'no-debugger': 'off',
-      'no-unused-vars': 'warn'
-    }
-  }
+  // Only apply TypeScript rules to .ts files
+  ...tseslint.configs.recommended.map(config => ({
+    ...config,
+    files: ['resources/**/*.ts']
+  }))
 ]
