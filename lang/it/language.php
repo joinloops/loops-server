c86fdaa6a7e5d24eed1f613e7d06b8e84851e0e8
dansup <danielsupernault@gmail.com>
2025-09-13 23:28:13 -0600

    New translations language.php (Italian)

--- lang/it/language.php
+++ lang/it/language.php
@@ -2,9 +2,9 @@
 
 return [
     'picker' => [
-        'title' => 'Select Language',
+        'title' => 'Seleziona lingua',
         'description' => 'Select your preferred language. This will change the language for the entire application.',
-        'select' => 'Select Language:',
+        'select' => 'Seleziona lingua:',
         'applyChanges' => 'Apply Changes',
         'current' => 'Current Language:',
         'note' => 'Your language preference will be saved automatically.',
