7b37dc1e07cf2a742671f6744c3799ca0cd218b2
dansup <danielsupernault@gmail.com>
2025-10-08 01:15:59 -0600

    New translations contact.php (Spanish)

--- lang/es/contact.php
+++ lang/es/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'adminContact' => 'Contacto administrativo',
+    'adminContact' => 'Contactar con el administrador',
     'adminContactMessage' => 'Para consultas relacionadas con asuntos operativos esenciales (utiliza el sistema de notificación integrado en la aplicación para problemas relacionados con el contenido).',
     'bodyMessage' => 'Estamos aquí para ayudarte. Ponte en contacto con nosotros a través de cualquiera de los canales que aparecen a continuación y te responderemos lo antes posible.',
     'communityForum' => 'Foro de la comunidad',
@@ -10,7 +10,7 @@ return [
     'getInTouch' => 'Ponerse en contacto',
     'loadingContactInformationDotDotDot' => 'Cargando información de contacto...',
     'responseTime' => 'Tiempo de respuesta',
-    'responseTimeMessage' => 'Normalmente respondemos a todas las consultas en un plazo de 24 horas durante los días laborables. Para cuestiones técnicas urgentes, incluya "URGENTE" en el asunto del mensaje.',
+    'responseTimeMessage' => 'Normalmente respondemos a todas las consultas en un plazo de 24 horas durante los días laborables. Para cuestiones técnicas urgentes, incluye "URGENTE" en el asunto del mensaje.',
     'socialConnect' => 'Conexión social',
     'socialConnectMessage' => 'Síguenos en el fediverso para recibir actualizaciones y realizar preguntas rápidas a través de mensajes directos.',
     'technicalSupport' => 'Soporte técnico',
