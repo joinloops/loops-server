e2bc8dcee4eceb0fc0bbc67dc2bde638392d3b62
dansup <danielsupernault@gmail.com>
2025-10-09 03:21:31 -0600

    New translations language.php (Greek)

--- lang/el/language.php
+++ lang/el/language.php
@@ -2,11 +2,11 @@
 
 return [
     'picker' => [
-        'title' => 'Select Language',
-        'description' => 'Select your preferred language. This will change the language for the entire application.',
-        'select' => 'Select Language:',
-        'applyChanges' => 'Apply Changes',
-        'current' => 'Current Language:',
-        'note' => 'Your language preference will be saved automatically.',
+        'title' => 'Επιλογή Γλώσσας',
+        'description' => 'Επέλεξε τη γλώσσα που προτιμάς. Αυτό θα αλλάξει τη γλώσσα για ολόκληρη την εφαρμογή.',
+        'select' => 'Επιλογή Γλώσσας:',
+        'applyChanges' => 'Εφαρμογή Αλλαγών',
+        'current' => 'Τρέχουσα Γλώσσα:',
+        'note' => 'Η προτίμηση γλώσσας σου θα αποθηκευτεί αυτόματα.',
     ],
 ];
