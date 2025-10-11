7b2ce4efa802e34d7d0d0bc30e379cbd4a908905
dansup <danielsupernault@gmail.com>
2025-10-11 08:13:36 -0600

    New translations language.php (Italian)

--- lang/it/language.php
+++ lang/it/language.php
@@ -5,8 +5,8 @@ return [
         'title' => 'Seleziona lingua',
         'description' => 'Select your preferred language. This will change the language for the entire application.',
         'select' => 'Seleziona lingua:',
-        'applyChanges' => 'Apply Changes',
-        'current' => 'Current Language:',
-        'note' => 'Your language preference will be saved automatically.',
+        'applyChanges' => 'Applica Modifiche',
+        'current' => 'Lingua attuale:',
+        'note' => 'Le tue preferenze sulla lingua verranno salvate automaticamente.',
     ],
 ];
