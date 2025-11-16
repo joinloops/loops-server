f5737ce8f9b6210e601e3b93e3eab611a54a9a24
dansup <danielsupernault@gmail.com>
2025-11-16 00:38:56 -0700

    New translations auth.php (Greek)

--- lang/el/auth.php
+++ lang/el/auth.php
@@ -3,5 +3,7 @@
 return [
     'failed' => 'Αυτά τα διαπιστευτήρια δεν ταιριάζουν με τα αρχεία μας.',
     'password' => 'Ο παρεχόμενος κωδικός πρόσβασης είναι λανθασμένος.',
+    'registrationsAreCurrentlyClosed' => 'Registrations are currently closed',
+    'thisServerIsntAccepting' => 'This server isn’t accepting new signups right now. Please check back later or choose another Loops server.',
     'throttle' => 'Παρα πολλές προσπάθειες σύνδεσης. Παρακαλώ δοκίμασε ξανά σε :seconds δευτερόλεπτα.',
 ];
