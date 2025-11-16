4ed404bfcb2c3d0aba43c20bde01dbbfc35900a3
dansup <danielsupernault@gmail.com>
2025-11-16 00:38:44 -0700

    New translations auth.php (Spanish)

--- lang/es/auth.php
+++ lang/es/auth.php
@@ -3,5 +3,7 @@
 return [
     'failed' => 'Estas credenciales no coinciden con nuestros registros.',
     'password' => 'La contraseña proporcionada es incorrecta.',
+    'registrationsAreCurrentlyClosed' => 'Registrations are currently closed',
+    'thisServerIsntAccepting' => 'This server isn’t accepting new signups right now. Please check back later or choose another Loops server.',
     'throttle' => 'Demasiados intentos de inicio de sesión. Vuelve a intentarlo en :seconds segundos.',
 ];
