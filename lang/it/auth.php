1d78cd914bf5098d03b060822f34c99394d0b223
dansup <danielsupernault@gmail.com>
2025-11-16 00:39:03 -0700

    New translations auth.php (Italian)

--- lang/it/auth.php
+++ lang/it/auth.php
@@ -3,5 +3,7 @@
 return [
     'failed' => 'Queste credenziali non risultano nel nostro sistema.',
     'password' => 'La password fornita non è corretta.',
+    'registrationsAreCurrentlyClosed' => 'Registrations are currently closed',
+    'thisServerIsntAccepting' => 'This server isn’t accepting new signups right now. Please check back later or choose another Loops server.',
     'throttle' => 'Troppi tentativi di accesso. Riprova tra :seconds secondi.',
 ];
