466c96012ca07eab9628e6ce3079cc8db522b599
dansup <danielsupernault@gmail.com>
2025-11-16 00:39:20 -0700

    New translations auth.php (Swedish)

--- lang/sv/auth.php
+++ lang/sv/auth.php
@@ -3,5 +3,7 @@
 return [
     'failed' => 'Inloggningsuppgifterna matchar inte de lagrade uppgifterna.',
     'password' => 'Det angivna lösenordet är felaktigt.',
+    'registrationsAreCurrentlyClosed' => 'Registrations are currently closed',
+    'thisServerIsntAccepting' => 'This server isn’t accepting new signups right now. Please check back later or choose another Loops server.',
     'throttle' => 'För många inloggningsförsök. Försök igen om :seconds sekunder.',
 ];
