963c2aabcbcdca1e0dd734f8b84906a27bf83d12
dansup <danielsupernault@gmail.com>
2025-11-25 05:58:13 -0700

    New translations auth.php (German)

--- lang/de/auth.php
+++ lang/de/auth.php
@@ -5,5 +5,5 @@ return [
     'password' => 'Das angegebene Passwort ist nicht korrekt.',
     'registrationsAreCurrentlyClosed' => 'Registrations are currently closed',
     'thisServerIsntAccepting' => 'This server isn’t accepting new signups right now. Please check back later or choose another Loops server.',
-    'throttle' => 'Es wurden zu viele Anmeldeversuche getätigt. Bitte versuchen Sie es wieder in :seconds Sekunden.',
+    'throttle' => 'Zu viele Anmeldeversuche. Bitte versuchen Sie es in :seconds Sekunden nochmal.',
 ];
