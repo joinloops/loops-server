f8b8f321e67933d13270b26934000b07cb75cfe1
dansup <danielsupernault@gmail.com>
2025-11-25 08:50:41 -0700

    New translations contact.php (German)

--- lang/de/contact.php
+++ lang/de/contact.php
@@ -2,7 +2,7 @@
 
 return [
     'adminContact' => 'Admin-Kontakt',
-    'adminContactMessage' => 'Für Anfragen im Zusammenhang mit operationellen Angelegenheiten (bitte verwenden Sie das In-App-Berichtssystem für inhaltliche Probleme).',
+    'adminContactMessage' => 'Für Anfragen im Zusammenhang mit essenziellen operationellen Angelegenheiten (bitte verwenden Sie das In-App-Berichtssystem für inhaltliche Probleme).',
     'bodyMessage' => 'Wir sind hier, um zu helfen. Bitte nutzen Sie einen der unten angegebenen Kanäle und wir werden uns so schnell wie möglich mit Ihnen in Verbindung setzen.',
     'communityForum' => 'Community-Forum',
     'communityForumMessage' => 'Treten Sie unserer Community-Diskussion bei, stellen Sie Fragen und teilen Sie Wissen mit anderen Nutzer:innen.',
