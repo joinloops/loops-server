82ae72697374724fe4fc407b8c89e24b84e704b0
dansup <danielsupernault@gmail.com>
2025-11-30 00:07:43 -0700

    New translations notifications.php (Spanish)

--- lang/es/notifications.php
+++ lang/es/notifications.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    'allCaughtUp' => '¡Ya estás al día!',
+    'allCaughtUp' => '¡Estás al día!',
     'errorLoadingNotifications' => 'Error al cargar las notificaciones',
     'messageTypes' => [
-        'videoLike' => 'le ha gustado tu vídeo',
+        'videoLike' => 'le ha gustado tu video',
         'newFollower' => 'ha comenzado a seguirte',
-        'videoComment' => 'ha comentado en tu vídeo',
+        'videoComment' => 'ha comentado en tu video',
         'videoCommentReply' => 'respondió a un comentario en tu vídeo',
         'videoCommentLike' => 'le dió me gusta a tu comentario',
         'videoCommentReplyLike' => 'le dió me gusta a tu respuesta',
         'videoCommentShare' => 'compartió tu comentario',
         'videoCommentReplyShare' => 'compartió tu respuesta',
-        'videoShare' => 'ha compartido tu vídeo',
+        'videoShare' => 'ha compartido tu video',
         'default' => 'ha interactuado con tu contenido',
     ],
     'noNotifications' => 'Sin notificaciones',
