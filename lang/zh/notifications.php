5db8ab4c307e57edbe91ee2b6c9b32b0bc5fa738
dansup <danielsupernault@gmail.com>
2025-11-30 00:07:55 -0700

    New translations notifications.php (Chinese Traditional)

--- lang/zh/notifications.php
+++ lang/zh/notifications.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    'allCaughtUp' => 'You\'re all caught up!',
-    'errorLoadingNotifications' => 'Error loading notifications',
+    'allCaughtUp' => '都读完了！',
+    'errorLoadingNotifications' => '加载通知时出错',
     'messageTypes' => [
-        'videoLike' => 'liked your video',
-        'newFollower' => 'started following you',
-        'videoComment' => 'commented on your video',
+        'videoLike' => '点赞了你的视频',
+        'newFollower' => '关注了你',
+        'videoComment' => '评论了你的视频',
         'videoCommentReply' => 'replied to a comment on your video',
         'videoCommentLike' => 'liked your comment',
         'videoCommentReplyLike' => 'liked your comment reply',
         'videoCommentShare' => 'shared your comment',
         'videoCommentReplyShare' => 'shared your comment reply',
-        'videoShare' => 'shared your video',
-        'default' => 'interacted with your content',
+        'videoShare' => '分享了你的视频',
+        'default' => '与你的内容进行了互动',
     ],
-    'noNotifications' => 'No notifications',
+    'noNotifications' => '暂无通知',
 ];
