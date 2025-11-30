7b936b1cc2e74e959c838d6944caf48ab0b4d93b
dansup <danielsupernault@gmail.com>
2025-11-30 00:07:19 -0700

    New translations language.php (Catalan)

--- lang/ca/language.php
+++ lang/ca/language.php
@@ -2,11 +2,11 @@
 
 return [
     'picker' => [
-        'title' => 'Select Language',
-        'description' => 'Select your preferred language. This will change the language for the entire application.',
-        'select' => 'Select Language:',
-        'applyChanges' => 'Apply Changes',
-        'current' => 'Current Language:',
-        'note' => 'Your language preference will be saved automatically.',
+        'title' => 'Selecciona idioma',
+        'description' => 'Selecciona el teu idioma preferit. Això canviarà l\'idioma de tota l\'aplicació.',
+        'select' => 'Selecciona idioma:',
+        'applyChanges' => 'Aplica els canvis',
+        'current' => 'Idioma actual:',
+        'note' => 'La teva preferència d\'idioma es desarà automàticament.',
     ],
 ];
