e7e1bd497968e6bbf0a934e1ea39c680dbd6c365
dansup <danielsupernault@gmail.com>
2025-11-30 00:06:26 -0700

    New translations auth.php (Korean)

--- lang/ko/auth.php
+++ lang/ko/auth.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => '인증 정보가 일치하지 않습니다.',
+    'password' => '비밀번호가 올바르지 않습니다.',
     'registrationsAreCurrentlyClosed' => 'Registrations are currently closed',
     'thisServerIsntAccepting' => 'This server isn’t accepting new signups right now. Please check back later or choose another Loops server.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'throttle' => '로그인 시도가 너무 많습니다. : 초 후 다시 시도하세요.',
 ];
