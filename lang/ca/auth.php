c8ab168c2dcab39f0c4249b12b8703fbd1b811cb
dansup <danielsupernault@gmail.com>
2025-11-30 00:06:22 -0700

    New translations auth.php (Catalan)

--- lang/ca/auth.php
+++ lang/ca/auth.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => 'Aquestes credencials no coincideixen amb els nostres registres.',
+    'password' => 'La contrasenya proporcionada és incorrecta.',
     'registrationsAreCurrentlyClosed' => 'Registrations are currently closed',
     'thisServerIsntAccepting' => 'This server isn’t accepting new signups right now. Please check back later or choose another Loops server.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'throttle' => 'Massa intents d\'inici de sessió. Si us plau, torna-ho a intentar d\'aquí a :seconds segons.',
 ];
