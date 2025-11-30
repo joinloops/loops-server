a29c0aa371db7ba05ac734ccdff27b89f5b835ce
dansup <danielsupernault@gmail.com>
2025-11-30 00:07:23 -0700

    New translations language.php (Korean)

--- lang/ko/language.php
+++ lang/ko/language.php
@@ -2,11 +2,11 @@
 
 return [
     'picker' => [
-        'title' => 'Select Language',
-        'description' => 'Select your preferred language. This will change the language for the entire application.',
-        'select' => 'Select Language:',
-        'applyChanges' => 'Apply Changes',
-        'current' => 'Current Language:',
-        'note' => 'Your language preference will be saved automatically.',
+        'title' => '언어 선택',
+        'description' => '선호하는 언어를 선택하세요. 선택하면 애플리케이션 전체 언어가 변경됩니다.',
+        'select' => '언어 선택:',
+        'applyChanges' => '변경 사항 적용',
+        'current' => '현재 언어:',
+        'note' => '언어 설정은 자동으로 저장됩니다.',
     ],
 ];
