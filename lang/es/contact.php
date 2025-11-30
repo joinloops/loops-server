1fb33f0030356e02278351bd9c44bb9458bd3cba
dansup <danielsupernault@gmail.com>
2025-11-30 00:06:51 -0700

    New translations contact.php (Spanish)

--- lang/es/contact.php
+++ lang/es/contact.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    'adminContact' => 'Contactar con el administrador',
-    'adminContactMessage' => 'Para consultas relacionadas con asuntos operativos esenciales (utiliza el sistema de notificación integrado en la aplicación para problemas relacionados con el contenido).',
-    'bodyMessage' => 'Estamos aquí para ayudarte. Ponte en contacto con nosotros a través de cualquiera de los canales que aparecen a continuación y te responderemos lo antes posible.',
+    'adminContact' => 'Contacto del administrador',
+    'adminContactMessage' => 'Para consultas relacionadas con asuntos operativos esenciales (usa el sistema de reportes en la app para problemas de contenido).',
+    'bodyMessage' => 'Estamos aquí para ayudarte. Contáctanos por cualquiera de los canales de abajo y te responderemos lo antes posible.',
     'communityForum' => 'Foro de la comunidad',
-    'communityForumMessage' => 'Únete a nuestros debates comunitarios, haz preguntas y comparte conocimientos con otros usuarios.',
-    'failedToLoadContactInformation' => 'No se ha podido cargar la información de contacto. Inténtalo de nuevo más tarde.',
+    'communityForumMessage' => 'Únete a las discusiones, haz preguntas y comparte conocimientos con otros usuarios.',
+    'failedToLoadContactInformation' => 'No se pudo cargar la información de contacto. Inténtalo de nuevo más tarde.',
     'getInTouch' => 'Ponerse en contacto',
     'loadingContactInformationDotDotDot' => 'Cargando información de contacto...',
     'responseTime' => 'Tiempo de respuesta',
-    'responseTimeMessage' => 'Normalmente respondemos a todas las consultas en un plazo de 24 horas durante los días laborables. Para cuestiones técnicas urgentes, incluye "URGENTE" en el asunto del mensaje.',
+    'responseTimeMessage' => 'Normalmente respondemos a todas las consultas en un plazo de 24 horas durante días laborables. Para asuntos técnicos urgentes, incluye "URGENTE" en el asunto.',
     'socialConnect' => 'Conexión social',
-    'socialConnectMessage' => 'Síguenos en el fediverso para recibir actualizaciones y realizar preguntas rápidas a través de mensajes directos.',
+    'socialConnectMessage' => 'Síguenos en el fediverso para actualizaciones y preguntas rápidas por mensaje directo.',
     'technicalSupport' => 'Soporte técnico',
-    'technicalSupportMessage' => '¿Necesitas ayuda con nuestra plataforma? Obtén asistencia técnica y soporte para la resolución de problemas.',
+    'technicalSupportMessage' => '¿Necesitas ayuda con nuestra plataforma? Obtén asistencia técnica y soporte de resolución de problemas.',
     'visitForum' => 'Visitar foro',
 ];
