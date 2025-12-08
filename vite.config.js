c19dfa8131e42c8a10b025c3088ebfabe34a7ad6
Shlee <github@shl.ee>
2025-12-08 21:09:42 +1030

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from "@tailwindcss/vite";
 import { resolve } from 'path'
 import { ensureDir, copy } from 'fs-extra'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    const assetCDNUrl = env.ASSETCDN_URL ? new URL(env.ASSETCDN_URL).origin : '';
-
-    return {
-        base: assetCDNUrl ? `${assetCDNUrl}/` : '/',
-        plugins: [
+export default defineConfig({
+    plugins: [
         laravel({
             input: [
                 'resources/js/app.js',
@@ -38,5 +33,4 @@ export default defineConfig(({ mode }) => {
     optimizeDeps: {
         exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
     }
-    };
 });
