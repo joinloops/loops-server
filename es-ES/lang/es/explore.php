5d2a2dc427c1d6157791cc2004fbc8e371852d44
dansup <danielsupernault@gmail.com>
2026-01-02 05:23:09 -0700

    New translations explore.php (Spanish)

---
+++ es-ES/lang/es/explore.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'discoverTrendingContent' => 'Descubre nuevo contenido en tendencia',
+    'noVideosFoundForThisHashtag' => 'No se han encontrado vídeos para esta etiqueta',
+];
