56b56b15823d0f6d5cdf289a72ce31bae4aaeb0c
dansup <danielsupernault@gmail.com>
2026-01-02 04:28:53 -0700

    New translations auth.php (French)

--- fr/lang/fr/auth.php
+++ fr/lang/fr/auth.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
+    'password' => 'Le mot de passe fourni est incorrect.',
+    'registrationsAreCurrentlyClosed' => 'Registrations are currently closed',
+    'thisServerIsntAccepting' => 'This server isn’t accepting new signups right now. Please check back later or choose another Loops server.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
 ];
