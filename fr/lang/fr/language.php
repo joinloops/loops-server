ca81dd0fa08fe856bd9103098a82708c92bf4071
dansup <danielsupernault@gmail.com>
2026-01-02 05:23:28 -0700

    New translations language.php (French)

---
+++ fr/lang/fr/language.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'picker' => [
+        'title' => 'Sélectionner la langue',
+        'description' => 'Choisissez votre langue préférée. Cela changera la langue de toute l\'application.',
+        'select' => 'Langue :',
+        'applyChanges' => 'Appliquer les changements',
+        'current' => 'Langue actuelle :',
+        'note' => 'Votre préférence linguistique sera enregistrée automatiquement.',
+    ],
+];
