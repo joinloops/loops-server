1917541e5b21a97f9db4c5fa9e00120fbc5733bf
dansup <danielsupernault@gmail.com>
2026-01-02 05:22:17 -0700

    New translations auth.php (Portuguese)

---
+++ pt-PT/lang/pt/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'failed' => 'Essas credenciais não correspondem aos nossos registos.',
+    'password' => 'A senha informada está incorreta.',
+    'registrationsAreCurrentlyClosed' => 'Os registos estão atualmente fechados',
+    'thisServerIsntAccepting' => 'Este servidor não aceita novas inscrições no momento. Por favor, volte mais tarde ou escolha outro servidor de laços.',
+    'throttle' => 'Demasiadas tentativas de acesso. Por favor, tente novamente.',
+];
