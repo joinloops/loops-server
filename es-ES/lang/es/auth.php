6447f509e81ac407d488929d508588f198c350c9
dansup <danielsupernault@gmail.com>
2026-01-02 05:22:05 -0700

    New translations auth.php (Spanish)

---
+++ es-ES/lang/es/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña introducida es incorrecta.',
+    'registrationsAreCurrentlyClosed' => 'Los registros están actualmente cerrados',
+    'thisServerIsntAccepting' => 'Este servidor no está aceptando nuevos registros ahora mismo. Por favor, revisa más tarde o elige otro servidor de Loops.',
+    'throttle' => 'Demasiados intentos de inicio de sesión. Por favor, inténtalo de nuevo en :seconds segundos.',
+];
