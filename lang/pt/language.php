e5afd271f1087c2775647a6452710f50e027b644
dansup <danielsupernault@gmail.com>
2026-01-05 13:03:41 -0700

    New translations language.php (Portuguese, Brazilian)

--- lang/pt/language.php
+++ lang/pt/language.php
@@ -2,14 +2,14 @@
 
 return [
     'choseYourPreferredLanguage' => 'Escolha o seu idioma preferido para a interface do Loops.',
-    'displayLanguage' => 'Idioma de exibição',
+    'displayLanguage' => 'Idioma de Exibição',
     'language' => 'Idioma',
     'picker' => [
-        'title' => 'Selecione o idioma',
+        'title' => 'Selecione o Idioma',
         'description' => 'Selecione o seu idioma preferido. Isso mudará o idioma de todo o aplicativo.',
-        'select' => 'Selecione o idioma:',
-        'applyChanges' => 'Aplicar alterações',
-        'current' => 'Idioma atual:',
+        'select' => 'Selecione o Idioma:',
+        'applyChanges' => 'Aplicar Alterações',
+        'current' => 'Idioma Atual:',
         'note' => 'Sua preferência de idioma será salva automaticamente.',
     ],
 ];
