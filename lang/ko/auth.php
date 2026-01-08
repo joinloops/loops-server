bbcd787a3b814ad934ac2def5822862e255ab87d
dansup <danielsupernault@gmail.com>
2026-01-08 04:47:50 -0700

    New translations auth.php (Korean)

--- lang/ko/auth.php
+++ lang/ko/auth.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
+    'byCreatingANewAccount' => 'By creating a new account, you agree to the',
+    'communityGuidelines' => 'Community Guidelines',
     'failed' => '인증 정보가 일치하지 않습니다.',
     'password' => '비밀번호가 올바르지 않습니다.',
+    'privacyPolicy' => 'Privacy Policy',
     'registrationsAreCurrentlyClosed' => 'Registrations are currently closed',
+    'termsOfService' => 'Terms of Service',
     'thisServerIsntAccepting' => 'This server isn’t accepting new signups right now. Please check back later or choose another Loops server.',
     'throttle' => '로그인 시도가 너무 많습니다. : 초 후 다시 시도하세요.',
 ];
