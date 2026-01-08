54d1cc04737933270e3424a85afbda60d6d78ee4
dansup <danielsupernault@gmail.com>
2026-01-08 04:47:36 -0700

    New translations auth.php (Ukrainian)

--- lang/uk/auth.php
+++ lang/uk/auth.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
+    'byCreatingANewAccount' => 'By creating a new account, you agree to the',
+    'communityGuidelines' => 'Community Guidelines',
     'failed' => 'Неправильна адреса електронної пошти або пароль.',
     'password' => 'Неправильний пароль.',
+    'privacyPolicy' => 'Privacy Policy',
     'registrationsAreCurrentlyClosed' => 'Реєстрація на даний час закрита',
+    'termsOfService' => 'Terms of Service',
     'thisServerIsntAccepting' => 'Цей сервер не приймає нових реєстрацій зараз. Будь ласка, перевірте пізніше або виберіть інший сервер Loops.',
     'throttle' => 'Забагато спроб входу. Повторіть спробу через :seconds секунд.',
 ];
