4030086becf6182a286abb178ca96634abdc3959
dansup <danielsupernault@gmail.com>
2026-01-08 04:47:31 -0700

    New translations auth.php (Catalan)

--- lang/ca/auth.php
+++ lang/ca/auth.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
+    'byCreatingANewAccount' => 'By creating a new account, you agree to the',
+    'communityGuidelines' => 'Community Guidelines',
     'failed' => 'Aquestes credencials no coincideixen amb els nostres registres.',
     'password' => 'La contrasenya proporcionada és incorrecta.',
+    'privacyPolicy' => 'Privacy Policy',
     'registrationsAreCurrentlyClosed' => 'Les inscripcions estan tancades ara mateix',
+    'termsOfService' => 'Terms of Service',
     'thisServerIsntAccepting' => 'El servidor no accepta inscripcions ara mateix. Si us plau, comprova-ho més tard o tria un altre servidor de Loops.',
     'throttle' => 'Massa intents d\'inici de sessió. Si us plau, torna-ho a intentar d\'aquí a :seconds segons.',
 ];
