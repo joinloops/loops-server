22325baf5bec86f7f12177654001d3e70aa2bf8f
dansup <danielsupernault@gmail.com>
2026-01-08 04:47:32 -0700

    New translations auth.php (German)

--- lang/de/auth.php
+++ lang/de/auth.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
+    'byCreatingANewAccount' => 'By creating a new account, you agree to the',
+    'communityGuidelines' => 'Community Guidelines',
     'failed' => 'Diese Zugangsdaten wurden nicht in unserer Datenbank gefunden.',
     'password' => 'Das eingegebene Passwort ist nicht korrekt.',
+    'privacyPolicy' => 'Privacy Policy',
     'registrationsAreCurrentlyClosed' => 'Registrierungen sind zurzeit geschlossen',
+    'termsOfService' => 'Terms of Service',
     'thisServerIsntAccepting' => 'Dieser Server akzeptiert derzeit keine neuen Registrierungen. Bitte überprüfen Sie später erneut oder wählen Sie einen anderen Loops-Server.',
     'throttle' => 'Zu viele Anmeldeversuche. Bitte versuchen Sie es in :seconds Sekunden nochmal.',
 ];
