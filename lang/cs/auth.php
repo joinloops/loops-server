4c4293b28c3ee23e2adda76d34edbcecb1aa1bea
dansup <danielsupernault@gmail.com>
2026-01-08 04:47:37 -0700

    New translations auth.php (Czech)

--- lang/cs/auth.php
+++ lang/cs/auth.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
+    'byCreatingANewAccount' => 'By creating a new account, you agree to the',
+    'communityGuidelines' => 'Community Guidelines',
     'failed' => 'Nesprávné přihlašovací údaje.',
     'password' => 'Zadané heslo je neplatné.',
+    'privacyPolicy' => 'Privacy Policy',
     'registrationsAreCurrentlyClosed' => 'Registrace jsou momentálně uzavřeny',
+    'termsOfService' => 'Terms of Service',
     'thisServerIsntAccepting' => 'Tento server momentálně nepřijímá nové registrace. Zkuste to prosím později nebo vyberte jiný Loops server.',
     'throttle' => 'Příliš mnoho pokusů o přihlášení. Zkuste to prosím znovu za :sekund sekund.',
 ];
