b3e50810d9abbbfdde7ed1a526a83d64dc0ada5b
dansup <danielsupernault@gmail.com>
2026-01-08 04:47:34 -0700

    New translations auth.php (Greek)

--- lang/el/auth.php
+++ lang/el/auth.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
+    'byCreatingANewAccount' => 'By creating a new account, you agree to the',
+    'communityGuidelines' => 'Community Guidelines',
     'failed' => 'Αυτά τα διαπιστευτήρια δεν ταιριάζουν με τα αρχεία μας.',
     'password' => 'Ο παρεχόμενος κωδικός πρόσβασης είναι λανθασμένος.',
+    'privacyPolicy' => 'Privacy Policy',
     'registrationsAreCurrentlyClosed' => 'Οι εγγραφές είναι κλειστές αυτή τη στιγμή',
+    'termsOfService' => 'Terms of Service',
     'thisServerIsntAccepting' => 'Αυτός ο διακομιστής δεν δέχεται νέες εγγραφές αυτή τη στιγμή. Παρακαλώ έλεγξε ξανά αργότερα ή επέλεξε έναν άλλο διακομιστή Loops.',
     'throttle' => 'Παρα πολλές προσπάθειες σύνδεσης. Παρακαλώ δοκίμασε ξανά σε :seconds δευτερόλεπτα.',
 ];
