26f55cf9e611de9870d00354c6b201e4a8c7ca17
dansup <danielsupernault@gmail.com>
2026-01-08 04:47:40 -0700

    New translations auth.php (Portuguese, Brazilian)

--- lang/pt/auth.php
+++ lang/pt/auth.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
+    'byCreatingANewAccount' => 'By creating a new account, you agree to the',
+    'communityGuidelines' => 'Community Guidelines',
     'failed' => 'Estas credenciais não estão nos nossos registros.',
     'password' => 'A senha fornecida está incorreta.',
+    'privacyPolicy' => 'Privacy Policy',
     'registrationsAreCurrentlyClosed' => 'As inscrições estão fechadas no momento',
+    'termsOfService' => 'Terms of Service',
     'thisServerIsntAccepting' => 'Este servidor não está aceitando novas inscrições no momento. Por favor, volte mais tarde ou escolha outro servidor de Repetições.',
     'throttle' => 'Você realizou muitas tentativas de \'login\'. Por favor, tente novamente em: \'segundos\' \'segundos\'.',
 ];
