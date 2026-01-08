bc909408784ca94b8967a3a12845cbac3e9f2935
dansup <danielsupernault@gmail.com>
2026-01-08 04:47:41 -0700

    New translations auth.php (Dutch)

--- lang/nl/auth.php
+++ lang/nl/auth.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
+    'byCreatingANewAccount' => 'By creating a new account, you agree to the',
+    'communityGuidelines' => 'Community Guidelines',
     'failed' => 'Deze inloggegevens komen niet overeen met onze database.',
     'password' => 'Het wachtwoord dat je hebt ingevoerd is niet correct.',
+    'privacyPolicy' => 'Privacy Policy',
     'registrationsAreCurrentlyClosed' => 'Registraties zijn momenteel gesloten',
+    'termsOfService' => 'Terms of Service',
     'thisServerIsntAccepting' => 'Deze server accepteert nu geen nieuwe aanmeldingen. Kom gerust later terug of kies een andere Loops server.',
     'throttle' => 'Te veel inlogpogingen. Opnieuw proberen kan over :seconds seconden.',
 ];
