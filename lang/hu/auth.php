0a4efa29d38c03eb783c6496cf7b7eda66930670
dansup <danielsupernault@gmail.com>
2026-01-08 04:47:48 -0700

    New translations auth.php (Hungarian)

--- lang/hu/auth.php
+++ lang/hu/auth.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
+    'byCreatingANewAccount' => 'By creating a new account, you agree to the',
+    'communityGuidelines' => 'Community Guidelines',
     'failed' => 'A megadott adatok nem egyeznek meg az adatbázisunkban szereplőkkel.',
     'password' => 'A megadott jelszó helytelen.',
+    'privacyPolicy' => 'Privacy Policy',
     'registrationsAreCurrentlyClosed' => 'Regisztráció jelenleg nem elérhető',
+    'termsOfService' => 'Terms of Service',
     'thisServerIsntAccepting' => 'Ez a szerver jelenleg nem engedélyez új regisztrálást. Nézz vissza később vagy válassz egy másik szervert.',
     'throttle' => 'Túl sok bejelentkezési kísérlet. Kérlek próbáld újra :seconds másodperc múlva.',
 ];
