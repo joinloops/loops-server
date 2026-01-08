9ed17d60e85bc1d8f58f7bdd121e0be38675bc3a
dansup <danielsupernault@gmail.com>
2026-01-08 04:47:29 -0700

    New translations auth.php (French)

--- lang/fr/auth.php
+++ lang/fr/auth.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
+    'byCreatingANewAccount' => 'By creating a new account, you agree to the',
+    'communityGuidelines' => 'Community Guidelines',
     'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
     'password' => 'Le mot de passe fourni est incorrect.',
+    'privacyPolicy' => 'Privacy Policy',
     'registrationsAreCurrentlyClosed' => 'Les inscriptions sont actuellement fermées',
+    'termsOfService' => 'Terms of Service',
     'thisServerIsntAccepting' => 'Ce serveur n\'accepte pas de nouvelles inscriptions pour le moment. Veuillez revenir plus tard ou choisir un autre serveur Loops.',
     'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
 ];
