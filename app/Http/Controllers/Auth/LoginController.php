fb6e95f628a242b72bac1dafbde754db80c9cd52
Shlee <github@shl.ee>
2026-01-09 23:07:14 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -110,7 +110,12 @@ class LoginController extends Controller
                     'string',
                     new HCaptchaRule(new CaptchaService),
                 ];
-            }
+            } else {
+            throw new \RuntimeException(
+                'Captcha is enabled but driver is not properly configured. ' .
+                'Set LOOPS_CAPTCHA_DRIVER to either "turnstile" or "hcaptcha".'
+            );
+          }
         }
 
         $request->validate($rules);
