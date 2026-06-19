90f32cd5baea4d1e81915c5766c91935614fbc33
dansup <danielsupernault@gmail.com>
2026-06-19 02:16:54 -0600

    New translations accounts.php (Finnish)
    [ci skip]

--- lang/fi/accounts.php
+++ lang/fi/accounts.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'addAnother' => 'Add another account',
-    'addingAnotherAccount' => 'Adding another account',
+    'addAnother' => 'Lisää toinen tili',
+    'addingAnotherAccount' => 'Lisätään toinen tili',
     'confirmLogoutAll' => 'Sign out of all linked accounts on this device?',
     'confirmRemove' => 'Remove :username from this device? You\'ll need to sign in again to switch back.',
     'logoutAll' => 'Sign out all accounts',
     'maxReached' => 'Maximum of :max accounts reached',
-    'removeAccount' => 'Remove from this device',
+    'removeAccount' => 'Poista tältä laitteelta',
+    'signOut' => 'Kirjaudu ulos tililtä',
     'signedInAs' => 'Signed in as',
-    'signOut' => 'Sign out of account',
-    'switchAccount' => 'Switch account',
+    'switchAccount' => 'Vaihda tiliä',
     'youCanSwitchAnytime' => 'You can switch back to your other accounts at any time.',
 ];
