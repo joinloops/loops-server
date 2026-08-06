5ba14aec5a9985b3088aaf1de3984c93939e066f
Shlee <github@shl.ee>
2026-08-06 20:34:04 +0930

    Update AcceptHandler.php

--- app/Federation/Handlers/AcceptHandler.php
+++ app/Federation/Handlers/AcceptHandler.php
@@ -7,6 +7,7 @@ use App\Models\FollowRequest;
 use App\Models\Profile;
 use App\Services\FollowerService;
 use App\Services\SanitizeService;
+use App\Services\ShareSheetService;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
